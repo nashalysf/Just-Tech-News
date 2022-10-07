c3719d3336926dd4f9f828aa95996b6f5505bbb8
nashalysf <nashalysf@gmail.com>
2022-10-06 22:36:12 -0400

    post routes created

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./user');
 const Post = require('./post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+//reverse association 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+});
+
 module.exports = { User, Post };
